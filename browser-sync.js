2e7d666c4bb63df805beaafc49379623f569274a
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-06-25 19:08:25 +0530

    intial commit

---
+++ browser-sync.js
@@ -0,0 +1,18 @@
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | Please report any issues you encounter:
+ |  https://github.com/shakyShane/browser-sync/issues
+ |
+ | For up-to-date information about the options:
+ |  https://github.com/shakyShane/browser-sync/wiki/Working-with-a-Config-File
+ |
+ */
+// eslint-disable-next-line no-undef
+module.exports = {
+    files: ["dist/css/*.css", "dist/js/*.js", "dist/**/*.html"],
+    serveStatic: ['dist'],
+    ghostMode: false,
+};
---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default [{
+  input: 'src/js/core/index.js',
+  output: {
+    file: 'dist/js/app.js',
+    format: 'cjs'
+  }
+}
+];
+
